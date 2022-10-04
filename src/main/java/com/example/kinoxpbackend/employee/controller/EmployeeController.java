f90ec640f221cfb9db4be16d60ea61fe91c481a1
Eliot996 <math10t6@stud.kea.dk>
2022-10-04 14:28:00 +0200

    KIN-27 update and delete working

--- src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
+++ src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import com.example.kinoxpbackend.employee.models.Employee;
 import com.example.kinoxpbackend.employee.repositories.EmployeeRepository;
 import com.example.kinoxpbackend.employee.services.EmployeeService;
 import com.example.kinoxpbackend.factory.DtoFactory;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,26 +19,15 @@ import java.util.Optional;
  */
 @CrossOrigin
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
-
-    private final EmployeeRepository repository;
 
     private final EmployeeService service;
 
     public EmployeeController(EmployeeRepository repository, EmployeeService service)
     {
-        this.repository = repository;
         this.service = service;
     }
-
-    //create employee @get og @post
-
-    @GetMapping("/create")
-    public String createEmployee(){
-        return "createEmployee";
-    }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDTO> find(@PathVariable("id") Long id) throws ResourceNotFoundException {
@@ -54,27 +42,30 @@ public class EmployeeController {
             return ResponseEntity.ok().body(DtoFactory.fromEmployee(item));
     }
 
-   /* @DeleteMapping("/{id}")
-    public ResponseEntity<Employee> delete(@PathVariable("id") Long id) {
+    @PatchMapping("/{id}")
+    public ResponseEntity<EmployeeDTO> update(@Valid @RequestBody Employee employee, @PathVariable("id") Long id){
+        employee.setId(id);
+        Employee item = service.update(employee);
+        return ResponseEntity.ok().body(DtoFactory.fromEmployee(item));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
         service.get(id).orElseThrow(() -> new ResourceNotFoundException("Customer %d not found.".formatted(id)));
 
         boolean delete = service.delete(id);
-        return
-    }*/
-
-
-    //delete employee @get og @post
-    @GetMapping("/delete")
-    public String deleteEmployee(){
-        return "deleteEmployee";
+        return ResponseEntity.ok().body("{deleted: " + delete + "}");
     }
+
+
     //update employee @get og @post
     @GetMapping("/update")
     public String updateEmployee(){
         return "updateEmployee";
     }
+
     //view employee @get og @post
-    @GetMapping("/")
+    @GetMapping()
     public ResponseEntity<List<EmployeeDTO>> viewAllEmployees(){
         return ResponseEntity.ok().body(DtoFactory.fromEmployees(service.getAll()));
     }
