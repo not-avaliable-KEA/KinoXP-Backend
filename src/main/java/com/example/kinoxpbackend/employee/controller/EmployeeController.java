f565bede6b57e3e4583e08f60f4712a9291ad0ae
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-10-04 12:27:53 +0200

    getMapping oprettet for CRUD Employee

--- src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
+++ src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
@@ -1,7 +1,41 @@
 package com.example.kinoxpbackend.employee.controller;
 
 
+import com.example.kinoxpbackend.employee.DTOs.EmployeeDTO;
+import com.example.kinoxpbackend.employee.models.Employee;
+import com.example.kinoxpbackend.employee.services.EmployeeService;
+import org.hibernate.result.UpdateCountOutput;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+/*
+@Author Sofia Abrahamsen + Lasse Bøgeskov-Jensen
+ */
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    EmployeeService service = new EmployeeService();
+    //create employee
+    @GetMapping("/create")
+    public String createEmployee(){
+        return "createEmployee";
+    }
+    //delete employee
+    @GetMapping("/delete")
+    public String deleteEmployee(){
+        return "deleteEmployee";
+    }
+    //update employee
+    @GetMapping("/update")
+    public String updateEmployee(){
+        return "updateEmployee";
+    }
+    //view employee
+    @GetMapping("/view")
+    public String viewEmployees(){
+        return "ViewEmployees";
+    }
 
 }
