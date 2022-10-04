d346bd5fd60e58950d60668580d52cb0c5709f67
Eliot996 <math10t6@stud.kea.dk>
2022-10-04 13:53:09 +0200

    KIN-27 added getAll

--- src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
+++ src/main/java/com/example/kinoxpbackend/employee/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 /*
@@ -73,9 +74,9 @@ public class EmployeeController {
         return "updateEmployee";
     }
     //view employee @get og @post
-    @GetMapping("/view")
-    public String viewEmployees(){
-        return "ViewEmployees";
+    @GetMapping("/")
+    public ResponseEntity<List<EmployeeDTO>> viewAllEmployees(){
+        return ResponseEntity.ok().body(DtoFactory.fromEmployees(service.getAll()));
     }
 
 }
