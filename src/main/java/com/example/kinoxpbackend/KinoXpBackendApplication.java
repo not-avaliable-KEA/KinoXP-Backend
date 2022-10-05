dac041e515affb6d89937a0bc731f6da8ad8d009
lass026q <lass026q@stud.kea.dk>
2022-10-05 10:35:58 +0200

    fix

--- src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
+++ src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
@@ -5,7 +5,6 @@ import com.example.kinoxpbackend.employee.services.EmployeeService;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
