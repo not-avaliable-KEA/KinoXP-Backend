2cdb9323907d4114b3dbaa78d44f8568b8c37a21
Eliot996 <math10t6@stud.kea.dk>
2022-10-07 09:49:58 +0200

    init

--- src/main/java/com/example/kinoxpbackend/movieTheater/models/MovieTheater.java
+++ src/main/java/com/example/kinoxpbackend/movieTheater/models/MovieTheater.java
@@ -1,7 +1,36 @@
 package com.example.kinoxpbackend.movieTheater.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="MovieTheaters")
+@Getter
+@Setter
+@NoArgsConstructor
 public class MovieTheater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    private String name;
+    private int rows;
+    private int columns;
 
+    public MovieTheater(String name, int rows, int columns) {
+        this.name = name;
+        this.rows = rows;
+        this.columns = columns;
+    }
 
+    public MovieTheater update(MovieTheater other) {
+        this.name = other.getName();
+        this.rows = other.getRows();
+        this.columns = other.getColumns();
+        return this;
+    }
 }
