1f755844c25d2dd1735c6a12fc2a4f0a67a4738e
OliverBark <oliver.bark@icloud.com>
2022-10-07 10:04:37 +0200

    MovieTheaterService done

--- src/main/java/com/example/kinoxpbackend/movieTheater/DTOs/MovieTheaterDTO.java
+++ src/main/java/com/example/kinoxpbackend/movieTheater/DTOs/MovieTheaterDTO.java
@@ -1,4 +1,14 @@
 package com.example.kinoxpbackend.movieTheater.DTOs;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class MovieTheaterDTO {
+
+
 }
--- src/main/java/com/example/kinoxpbackend/movieTheater/services/MovieTheaterService.java
+++ src/main/java/com/example/kinoxpbackend/movieTheater/services/MovieTheaterService.java
@@ -1,4 +1,53 @@
 package com.example.kinoxpbackend.movieTheater.services;
 
+
+
+import com.example.kinoxpbackend.movieTheater.models.MovieTheater;
+import com.example.kinoxpbackend.movieTheater.repositories.MovieTheaterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MovieTheaterService {
+
+
+    @Autowired
+    MovieTheaterRepository repository;
+
+    public MovieTheater create(MovieTheater movieTheater) {
+        return repository.save(movieTheater);
+    }
+
+
+    public List<MovieTheater> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<MovieTheater> get(long id){
+        return repository.findById(id);
+    }
+
+    public MovieTheater update(MovieTheater movieTheater) {
+        Optional<MovieTheater> optionalMovieTheater = get(movieTheater.getId());
+
+        if (optionalMovieTheater.isEmpty()) return null;
+        else {
+            optionalMovieTheater.get().update(movieTheater);
+        }
+        return repository.save(optionalMovieTheater.get());
+    }
+
+    public Boolean delete (long id){
+        Optional<MovieTheater> optionalMovieTheater = get(id);
+
+        if (optionalMovieTheater.isPresent()){
+            repository.delete(optionalMovieTheater.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
