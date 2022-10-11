712be2ea03a009ccb8bd40880b26ec0a253764a0
Eliot996 <math10t6@stud.kea.dk>
2022-10-11 14:05:50 +0200

    added services to DtoFactory

--- src/main/java/com/example/kinoxpbackend/factory/DtoFactory.java
+++ src/main/java/com/example/kinoxpbackend/factory/DtoFactory.java
@@ -5,10 +5,16 @@ import com.example.kinoxpbackend.employee.DTOs.EmployeeDTO;
 import com.example.kinoxpbackend.employee.models.Employee;
 import com.example.kinoxpbackend.movie.DTOs.MovieDTO;
 import com.example.kinoxpbackend.movie.models.Movie;
+import com.example.kinoxpbackend.movie.services.MovieService;
+import com.example.kinoxpbackend.movieListing.DTO.MovieListingDTO;
+import com.example.kinoxpbackend.movieListing.models.MovieListing;
 import com.example.kinoxpbackend.movieTheater.DTOs.MovieTheaterDTO;
 import com.example.kinoxpbackend.movieTheater.models.MovieTheater;
+import com.example.kinoxpbackend.movieTheater.services.MovieTheaterService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +22,21 @@ public class DtoFactory
 {
     private static ModelMapper modelMapper = KinoXpBackendApplication.modelMapper();
 
+    private static MovieService movieService;
+
+    private static MovieTheaterService movieTheaterService;
+
+    public static void setMovieService(MovieService movieService) {
+        DtoFactory.movieService = movieService;
+    }
+
+    public static void setMovieTheaterService(MovieTheaterService movieTheaterService) {
+        DtoFactory.movieTheaterService = movieTheaterService;
+    }
 
     /*
-        Employees
-    */
+            Employees
+        */
     public static EmployeeDTO fromEmployee(Employee employee)
     {
         return modelMapper.map(employee, EmployeeDTO.class);
@@ -62,20 +79,34 @@ public class DtoFactory
                 .collect(Collectors.toList());
     }
 
+    public static MovieListingDTO fromMovieListing(MovieListing movieListing)
+    {
+        return modelMapper.map(movieListing, MovieListingDTO.class);
+    }
 
+    public static List<MovieListingDTO> fromMovieListings(List<MovieListing> movieListings){
+        return movieListings.stream()
+                .map(movieListing -> modelMapper.map(movieListing, MovieListingDTO.class))
+                .collect(Collectors.toList());
+    }
 
+    public static MovieListing fromMovieListingDTO(MovieListingDTO movieListingDTO) {
+    // oprette ny tom movielisting
+        MovieListing movieListing = new MovieListing();
 
+        // overføre id og date dertil.
+        movieListing.setId(movieListingDTO.getId());
+        //movieListing.setDate(Date.from(movieListingDTO.getDate()));
 
+        // finde movie udefra movie id, og overføre til movielisting.
 
+        movieListing.setMovie(movieService.get(movieListingDTO.getMovieId()).get());
 
+        //det samme med movie theater.
+        movieListing.setMovieTheater(movieTheaterService.get(movieListingDTO.getMovieTheaterId()).get());
 
-
-
-
-
-
-
-
-
+        //returnerer movielisting.
+        return movieListing;
+    }
 
 }
