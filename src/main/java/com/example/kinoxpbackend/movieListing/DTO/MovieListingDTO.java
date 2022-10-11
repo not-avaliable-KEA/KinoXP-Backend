11ef29d021582decfc38db856af4fdbea338d596
Eliot996 <math10t6@stud.kea.dk>
2022-10-11 14:06:50 +0200

    changed DTO to only take id's from movie and movietheater

--- src/main/java/com/example/kinoxpbackend/movieListing/DTO/MovieListingDTO.java
+++ src/main/java/com/example/kinoxpbackend/movieListing/DTO/MovieListingDTO.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class MovieListingDTO {
 
     private Long id;
-    private Movie movie;
-    private MovieTheater movieTheater;
-    private Date date;
+    private Long movieId;
+    private Long movieTheaterId;
+    private String date;
 }
